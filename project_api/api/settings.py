2ec16cf89691f150acc1d4a1d137f29f5315297a
Ekaterina Chernysheva <knopa-f1@ya.ru>
2025-02-09 20:22:16 +0200

    fix: ALLOWED_HOSTS to the settings

--- project_api/api/settings.py
+++ project_api/api/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["127.0.0.1",
-                 "authentication_api.onrender.com"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
 
 
 # Application definition
