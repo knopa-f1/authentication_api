c9f79602f4645d589b6870e41585319bc6ab8e36
Ekaterina Chernysheva <knopa-f1@ya.ru>
2025-02-09 12:34:19 +0200

    feature: admin panel + requerements

--- api/core/admin.py
+++ api/core/admin.py
@@ -5,9 +5,14 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, UserRefreshToken
 
 admin.site.register(User, UserAdmin)
-
+admin.site.site_header = "RESTful API for Authentication admin webpage"
+admin.site.site_title = "RESTful API for Authentication admin webpage"
 
 @admin.register(UserRefreshToken)
 class RefreshTokenAdmin(admin.ModelAdmin):
     list_display = ('user', 'token')
+
+
+
+
 
